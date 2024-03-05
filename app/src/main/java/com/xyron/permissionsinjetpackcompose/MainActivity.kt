466745205f98b36c7a5d58357a13ac4a69b4405c
Muhammad Tayyab <muhammadtayyab703@gmail.com>
2024-03-05 23:04:43 +0400

    Practiced another Permission method

--- app/src/main/java/com/xyron/permissionsinjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/xyron/permissionsinjetpackcompose/MainActivity.kt
@@ -4,8 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.Manifest
 import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.provider.Settings
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
@@ -18,10 +21,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.xyron.permissionsinjetpackcompose.ui.theme.PermissionsInJetpackComposeTheme
 
@@ -33,16 +42,16 @@ class MainActivity : ComponentActivity() {
                 val viewModel = viewModel<MainViewModel>()
                 val dialogQueue = viewModel.visiblePermissionDialogQueue
 
-               /* val cameraPermissionResultLauncher = rememberLauncherForActivityResult(
-                    contract = ActivityResultContracts.RequestPermission,
-                    onResult = { isGranted ->
+                /* val cameraPermissionResultLauncher = rememberLauncherForActivityResult(
+                     contract = ActivityResultContracts.RequestPermission,
+                     onResult = { isGranted ->
 
-                        viewModel.onPermissionResult(
-                            permission = Manifest.permission.CAMERA,
-                            isGranted = isGranted
-                        )
-                    }
-                )*/
+                         viewModel.onPermissionResult(
+                             permission = Manifest.permission.CAMERA,
+                             isGranted = isGranted
+                         )
+                     }
+                 )*/
 
 
                 val resultCallback: (Boolean) -> Unit = { isGranted ->
@@ -89,7 +98,7 @@ class MainActivity : ComponentActivity() {
                     }) {
                         Text(text = "Request one permission")
                     }
-                    
+
                     Spacer(modifier = Modifier.height(16.dp))
 
                     Button(onClick = {
@@ -105,29 +114,33 @@ class MainActivity : ComponentActivity() {
                 }
 
                 dialogQueue
-                    .forEach { permission -> 
+                    .forEach { permission ->
                         PermissionDialog(
                             permissionTextProvider = when (permission) {
                                 Manifest.permission.CAMERA -> {
                                     CameraPermissionTextProvider()
                                 }
+
                                 Manifest.permission.RECORD_AUDIO -> {
                                     AudioPermissionTextProvider()
                                 }
+
                                 Manifest.permission.CALL_PHONE -> {
                                     PhoneCallPermissionTextProvider()
                                 }
+
                                 else -> return@forEach
                             },
                             isPermanentlyDeclined = !shouldShowRequestPermissionRationale(permission),
                             onDismiss = viewModel::dismissDialog,
                             onOkClick = {
-                                        viewModel.dismissDialog()
+                                viewModel.dismissDialog()
                                 multiplePermissionResultLauncher.launch(
                                     arrayOf(permission)
                                 )
                             },
-                            onGoToAppSettingsClick = ::openAppSettings)
+                            onGoToAppSettingsClick = ::openAppSettings
+                        )
                     }
 
             }
@@ -136,7 +149,7 @@ class MainActivity : ComponentActivity() {
 }
 
 
-fun Activity.openAppSettings(){
+fun Activity.openAppSettings() {
 
     Intent(
         Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
@@ -145,17 +158,46 @@ fun Activity.openAppSettings(){
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Greeting() {
+
+    val context = LocalContext.current
+    val cameraPermission = Manifest.permission.CAMERA
+    var isCameraPermissionGranted by remember {
+        mutableStateOf(checkPermissionFor(context = context, permission = cameraPermission))
+    }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { isGranted ->
+
+            if (isGranted) {
+                Log.d("TAG", "Camera $isGranted")
+                isCameraPermissionGranted = true
+            }
+
+        }
     )
+
+    Column {
+        Button(onClick = {
+            if (!isCameraPermissionGranted) {
+                launcher.launch(cameraPermission)
+            }
+
+        }) {
+            Text(text = "Camera Permission $isCameraPermissionGranted")
+        }
+    }
+}
+
+private fun checkPermissionFor(context: Context, permission: String): Boolean {
+    return ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PermissionsInJetpackComposeTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
